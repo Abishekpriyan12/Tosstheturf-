267c1d93b1f15b575a567a74f4ad5dbfe9d15e83
viswa1220 <viswanathan122000@gmail.com>
2024-09-24 11:36:28 -0400

    icon button recreated

--- client/src/components/Button-Component/ButtonComponent.js
+++ client/src/components/Button-Component/ButtonComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './ButtonComponent.css'
-const ButtonComponent = ({btnName}) => {
+const ButtonComponent = ({btnName,iconPath}) => {
     return(<div className="button">
-        <button>{btnName}</button>
+        <button>{iconPath && <img src={iconPath} alt={`${btnName} icon`}></img>}{btnName}</button>
     </div>)
 };
 export default ButtonComponent;
--- client/src/components/Home-Component/HomePageComponent.js
+++ client/src/components/Home-Component/HomePageComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import ButtonComponent from "../Button-Component/ButtonComponent";
 const HomePageComponent = () => {
     return(<div className="home-page">
+        <ButtonComponent btnName="Login"></ButtonComponent>
     </div>)
 };
 export default HomePageComponent;
