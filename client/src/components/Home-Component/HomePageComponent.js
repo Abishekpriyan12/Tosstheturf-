050dbfcb1d4bb5afa06cb9242b83b15a14b71836
viswa1220 <viswanathan122000@gmail.com>
2024-09-24 12:34:22 -0400

    data issue modified

--- client/src/components/Home-Component/HomePageComponent.js
+++ client/src/components/Home-Component/HomePageComponent.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import ButtonComponent from "../Button-Component/ButtonComponent";
 const HomePageComponent = () => {
-    const [data] = useState(['abishek', 'vish', 'sasi'])
-    const [dummy,setdummy] = useState(data)
-    return(<div className="home-page">
-        <ButtonComponent iconPath="user.png" btnName="Login"></ButtonComponent>
-        <ul>
-       { dummy.map((item,index)=>{
-        console.log(item); 
-
-      return <li key={index}>{item}</li>
-                
-            
-        })}
-        </ul>
-    </div>)
+  const [data] = useState(["abishek", "vish", "sasi"]);
+  const [dummy, setdummy] = useState(data);
+  return (
+    <div className="home-page">
+      <ButtonComponent iconPath="user.png" btnName="Login"></ButtonComponent>
+      <ul>
+        {dummy.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default HomePageComponent;
