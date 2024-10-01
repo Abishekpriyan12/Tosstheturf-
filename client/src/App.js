73b2f164718bd3487aab1da5cdc2ded3dc6484cc
AkshaParvadiya <akshaparvadiya123@gmail.com>
2024-10-01 00:30:05 -0400

    About Us Page Done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageComponent from './components/Home-Component/HomePageComponent';
 import ContactUsComponent from './components/ContactUs-Component/ContactUsComponent'
+import AboutUsComponent from './components/AboutUs-Component/AboutUsComponent';
 
 
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePageComponent />} />
           <Route path="/contact" element={<ContactUsComponent></ContactUsComponent>} />
+          <Route path="/about" element={<AboutUsComponent></AboutUsComponent>} />
         </Routes>
       </div>
     </Router>
