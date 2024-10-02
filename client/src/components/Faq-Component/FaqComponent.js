7d3ddeaaa8f8ab8c0acb9d162d79bb984ff02e0a
Surajmehta10 <mehtasuraj2000@gmail.com>
2024-10-01 22:32:58 -0400

    added faq componentand design

--- client/src/components/Faq-Component/FaqComponent.js
+++ client/src/components/Faq-Component/FaqComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import NavBarComponent from "../navigation-component/NavBarComponent";
 import FooterComponent from "../footer-component/FooterComponent";
+import CardComponent from "../Card-Component/CardComponent";
 import "./FaqComponent.css";
 
 const FaqComponent = () => {
@@ -14,29 +15,56 @@ const FaqComponent = () => {
   const faqData = [
     {
       question: "How do I book a turf?",
-      answer:
-       <p>"To book a turf, simply select your preferred sport, <br/>choose a venue, pick a time slot, and complete the booking through our secure payment system.",</p>
+      answer: (
+        <p>
+          "To book a turf, simply select your preferred sport, <br />
+          choose a venue, pick a time slot, and complete the booking through our
+          secure payment system.",
+        </p>
+      ),
     },
     {
       question: "Can I cancel or reschedule my booking?",
-      answer:
-       <p> "Yes, you can cancel or reschedule your booking up to 24 hours before your scheduled time.<br/> Visit your account page and select the booking you want to modify.",</p>
+      answer: (
+        <p>
+          {" "}
+          "Yes, you can cancel or reschedule your booking up to 24 hours before
+          your scheduled time.
+          <br /> Visit your account page and select the booking you want to
+          modify.",
+        </p>
+      ),
     },
     {
       question: "Do I need to bring my own equipment?",
-      answer:
-        <p>"Most venues provide basic equipment like nets and goals.<br/> However, players are encouraged to bring personal equipment like rackets,balls,or shoes.",</p>
+      answer: (
+        <p>
+          "Most venues provide basic equipment like nets and goals.
+          <br /> However, players are encouraged to bring personal equipment
+          like rackets,balls,or shoes.",
+        </p>
+      ),
     },
     {
       question: "Are group bookings allowed?",
-      answer:
-        <p>"Yes! You can book the turf for individual or group sessions. <br/>For larger groups or special events, contact us for custom booking options.",</p>
+      answer: (
+        <p>
+          "Yes! You can book the turf for individual or group sessions. <br />
+          For larger groups or special events, contact us for custom booking
+          options.",
+        </p>
+      ),
     },
     {
       question:
         "What happens if the turf is unavailable during my booked time?",
-      answer:
-        <p>"In the rare event of turf unavailability,<br/> we will notify you immediately and provide options to reschedule or receive a full refund.",</p>
+      answer: (
+        <p>
+          "In the rare event of turf unavailability,
+          <br /> we will notify you immediately and provide options to
+          reschedule or receive a full refund.",
+        </p>
+      ),
     },
   ];
   const [activeIndex, setActiveIndex] = useState(null);
@@ -44,30 +72,32 @@ const FaqComponent = () => {
   return (
     <>
       <NavBarComponent navBarData={navBarData} />
-      <div className="faq-container">
-        <h1>Frequently Asked Questions</h1>
-        <div className="accordion">
-          {faqData.map((faq, index) => (
-            <div key={index} className="accordion-item">
-              <button
-                className={`accordion-button ${
-                  activeIndex === index ? "active" : ""
-                }`}
-                onClick={() =>
-                  setActiveIndex(activeIndex === index ? null : index)
-                }
-              >
-                {faq.question}
-              </button>
-              {activeIndex === index && (
-                <div className="accordion-content">
-                  <p>{faq.answer}</p>
-                </div>
-              )}
-            </div>
-          ))}
+      <CardComponent width="60%" height="500px">
+        <div className="faq-container">
+          <h1>Frequently Asked Questions</h1>
+          <div className="accordion">
+            {faqData.map((faq, index) => (
+              <div key={index} className="accordion-item">
+                <button
+                  className={`accordion-button ${
+                    activeIndex === index ? "active" : ""
+                  }`}
+                  onClick={() =>
+                    setActiveIndex(activeIndex === index ? null : index)
+                  }
+                >
+                  {faq.question}
+                </button>
+                {activeIndex === index && (
+                  <div className="accordion-content">
+                    <p>{faq.answer}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </CardComponent>
       <FooterComponent />
     </>
   );
