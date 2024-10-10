54abcb530102c2c08857a92a0f062cb17053c2cb
viswa1220 <viswanathan122000@gmail.com>
2024-10-09 21:39:29 -0400

    Navigation bar dropdown button

--- client/src/components/navigation-component/NavBarComponent.js
+++ client/src/components/navigation-component/NavBarComponent.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBarComponent.css";
-import ButtonComponent from "../Button-Component/ButtonComponent";
 import { Link } from "react-router-dom";
 
 const NavBarComponent = ({ navBarData }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleOptionClick = (role) => {
+    console.log(`Navigate to login as ${role}`);
+  };
+
   return (
     <div className="Nav-bar">
       <div className="card-container">
         <div className="Logo">
           <img src="ttt_logo.png" alt="logo" />
         </div>
-
-        <div id="location-input">
-          <img
-            src="/assests/icons/user.png"
-            alt="location"
-            className="location-icon"
-          />
-          <input
-            type="text"
-            placeholder="Enter location"
-            className="location-textbox"
-          />
-        </div>
-
+        <div id="location-input" />
         <div className="nav-links">
           <ul>
-            {
-              navBarData.map((item) => (
-                <li key={item.id}>
-                  <Link to={item.url}>{item.name}</Link>
-                </li>
-              ))
-            }
-            <div id="button-comp">
-              <ButtonComponent btnName="Login" iconPath="User.png" />
+            {navBarData.map((item) => (
+              <li key={item.id}>
+                <Link to={item.url}>{item.name}</Link>
+              </li>
+            ))}
+            <div id="button-comp" className="dropdown">
+              <button className="dropdown-button" onClick={toggleDropdown}>
+                Login
+              </button>
+              {dropdownOpen && (
+                <div className="dropdown-menu">
+                  <div className="dropdown-item" onClick={() => handleOptionClick("Admin")}>Admin</div>
+                  <div className="dropdown-item" onClick={() => handleOptionClick("User")}>User</div>
+                  <div className="dropdown-item" onClick={() => handleOptionClick("Owner")}>Owner</div>
+                </div>
+              )}
             </div>
           </ul>
         </div>
