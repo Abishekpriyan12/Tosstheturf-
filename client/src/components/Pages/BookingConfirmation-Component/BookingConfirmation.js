b4b067b7700e6118e66f3fe12c3290ecfc500a93
unknown <abishekpriyan11@gmail.com>
2024-12-12 01:25:20 -0500

    Css Corrections

--- client/src/components/Pages/BookingConfirmation-Component/BookingConfirmation.js
+++ client/src/components/Pages/BookingConfirmation-Component/BookingConfirmation.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation,useNavigate} from "react-router-dom";
 import NavBarComponent from "../../Reusable-Components/navigation-component/NavBarComponent";
 import FooterComponent from "../../Reusable-Components/footer-component/FooterComponent";
 import ButtonComponent from "../../Reusable-Components/Button-Component/ButtonComponent";
 import CardComponent from "../../Reusable-Components/Card-Component/CardComponent";
 import ScrollerComponent from "../../Reusable-Components/Scroller-Component/ScrollerComponent";
 import "./BookingConfirmation.css";
+import turfimg from "../../../assests/icons/field.png"
+import cashimg from "../../../assests/icons/cash.png"
+import calendar from "../../../assests/icons/calendar.png"
+
+
+
+
+
+
 
 const BookingConfirmation = () => {
   // Access the booking details passed from PaymentComponent
   const location = useLocation();
   const bookingDetails = location.state;
+  console.log("bookingstate",bookingDetails)
+  const userId = sessionStorage.getItem("userId");
+  const navigate = useNavigate();
+
+
+
 
   // Placeholder for recommended turfs
   const items = [
@@ -66,49 +81,43 @@ const BookingConfirmation = () => {
       <div className="custom-booking-confirmation-container">
         <section className="custom-booking-success-section">
           <div className="custom-booking-header-image">
-            <img src="./assests/images/golf.jpg" alt="Booking Success" />
+        
             <h1>Your booking has been done successfully!!</h1>
           </div>
         </section>
 
         {/* Booking Details */}
         <section className="custom-booking-details-wrapper">
-          <CardComponent className="custom-booking-card">
+          <CardComponent className="custom-booking-card" width={'40%'}>
             <h3>Booking Details</h3>
             <div className="custom-booking-info">
               <div className="custom-booking-item">
                 <img src="./assests/icons/user.png" alt="User Icon" />
-                <p className="custom-user-name">{bookingDetails.userName}</p>
+                <p className="custom-user-name">{userId}</p>
               </div>
+            
               <div className="custom-booking-item">
-                <img src="./assests/icons/location.png" alt="Location Icon" />
-                <p className="custom-location-details">
-                  {bookingDetails.location}
-                </p>
-              </div>
-              <div className="custom-booking-item">
-                <img src="./assests/icons/soccer.png" alt="Turf Icon" />
+                <img src={turfimg} alt="Turf Icon" />
                 <p className="custom-turf-details">{bookingDetails.turfName}</p>
               </div>
               <div className="custom-booking-item">
-                <img src="./assests/icons/calendar.png" alt="Calendar Icon" />
+                <img src={calendar} alt="Calendar Icon" />
                 <p className="custom-date-time">
                   {bookingDetails.date}, {bookingDetails.time.join(", ")}
                 </p>
               </div>
               <div className="custom-booking-item">
-                <img src="./assests/icons/money.png" alt="Cost Icon" />
+                <img src={cashimg} alt="Cost Icon" />
                 <p className="custom-cost-details">${bookingDetails.price}</p>
               </div>
             </div>
             <div className="custom-button-container">
-              <ButtonComponent btnName="Book more Turfs" />
+              <ButtonComponent btnName="Book more Turfs" onClick={() => navigate("/turfSearch")}/>
             </div>
           </CardComponent>
         </section>
 
-        {/* Recommendations Scroller */}
-        <ScrollerComponent items={items} />
+  
 
         {/* Footer */}
         <FooterComponent />
